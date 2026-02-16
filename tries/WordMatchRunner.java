571ece7fd8775dddf92de267bbeacf173843aa01
amrit rajput <vishal179singh@gmail.com>
2026-02-16 20:09:42 +0530

    fix runner

--- tries/WordMatchRunner.java
+++ tries/WordMatchRunner.java
@@ -3,8 +3,8 @@ package tries;
 public class WordMatchRunner {
     public static void main(String[] args) {
         BuildTrie trie = new BuildTrie();
-        trie.preProcess("this is amrit, he is in 6th sem, he/him");
-        boolean ans = WordMatcher.isWordMatched(trie,"he/him");
+        trie.preProcess("this is amrit, he is in 6th sem, he/him he-she");
+        boolean ans = WordMatcher.isWordMatched(trie,"he-she");
         System.out.println(ans);
     }
 }
