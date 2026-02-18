818fa9f024217838d95a94ee4514e80cf0d94dd8
amrit rajput <vishal179singh@gmail.com>
2026-02-19 00:31:57 +0530

    calculate f(x)

--- recursion/Calculate.java
+++ recursion/Calculate.java
@@ -1,5 +1,12 @@
 public class Calculate{
     public static void main(String[] args) {
-        
+        System.out.println (calculate(100));
+    }
+    private static long calculate(int x) {
+        if(x<10){
+            return 1;
+        }
+        long ans = ((long) Math.pow((2.0*x), 3.0)) + calculate(x-1) + calculate(x-3) ;
+        return ans;
     }
 }
