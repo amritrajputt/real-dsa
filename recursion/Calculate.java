4612af601512954a72f1527aebc17609c9545f94
amrit rajput <vishal179singh@gmail.com>
2026-02-19 22:48:54 +0530

    optimized f(x) problem using DP

--- recursion/Calculate.java
+++ recursion/Calculate.java
@@ -1,12 +1,20 @@
+import java.util.*;
 public class Calculate{
+    static long dp[];
     public static void main(String[] args) {
-        System.out.println (calculate(100));
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        dp = new long[n+1];
+        Arrays.fill(dp,-1);
+        System.out.println (calculate(n));
     }
     private static long calculate(int x) {
         if(x<10){
             return 1;
         }
+        if(dp[x] != -1) return dp[x];
         long ans = ((long) Math.pow((2.0*x), 3.0)) + calculate(x-1) + calculate(x-3) ;
+        dp[x] = ans;
         return ans;
     }
 }
