3290d3981a521cd5d0b583b86bb1a97f687d9a04
amrit rajput <vishal179singh@gmail.com>
2026-02-20 17:50:02 +0530

    use BigInteger class to make it robust for bigger value of x

--- recursion/Calculate.java
+++ recursion/Calculate.java
@@ -1,19 +1,30 @@
 import java.util.*;
+import java.math.*;
 public class Calculate{
-    static long dp[];
+    static BigInteger dp[];
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of x: ");
         int n = sc.nextInt();
-        dp = new long[n+1];
-        Arrays.fill(dp,-1);
+        dp = new BigInteger[n+1];
+        Arrays.fill(dp,null);
         System.out.println (calculate(n));
     }
-    private static long calculate(int x) {
+    
+    private static BigInteger calculate(int x) {
+        
         if(x<10){
-            return 1;
+            return BigInteger.ONE;
         }
-        if(dp[x] != -1) return dp[x];
-        long ans = ((long) Math.pow((2.0*x), 3.0)) + calculate(x-1) + calculate(x-3) ;
+
+        if(dp[x] != null) return dp[x];
+
+        BigInteger cube = BigInteger.valueOf(8L)
+                        .multiply(BigInteger.valueOf(x))
+                        .multiply(BigInteger.valueOf(x))
+                        .multiply(BigInteger.valueOf(x));
+
+        BigInteger ans = cube.add(calculate(x-1)).add(calculate(x-3)) ;
         dp[x] = ans;
         return ans;
     }
