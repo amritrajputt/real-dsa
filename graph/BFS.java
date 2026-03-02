55538f860d7e1185335ed588230ecc60b66bfef7
amrit <vishal179singh@gmail.com>
2026-03-02 20:15:21 +0530

    BFS DFS in graph

--- graph/BFS.java
+++ graph/BFS.java
@@ -6,54 +6,82 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class BFS {
-    public static void main(String[] args) {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        Scanner scn = new Scanner(System.in);
-        System.out.println("Enter number of vertices: ");
-        int rows = scn.nextInt();
-        scn.close();
 
-        for (int i = 0; i <= rows; i++) {
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Enter number of vertices: ");
+        int nodes = scn.nextInt();
+
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        for (int i = 0; i <= nodes; i++) {
             adj.add(new ArrayList<>());
         }
 
-        addList(adj, 1, 2);
-        addList(adj, 1, 4);
-        addList(adj, 3, 4);
-        addList(adj, 2, 3);
-        addList(adj, 7, 2);
-        addList(adj, 5, 4);
-        addList(adj, 3, 7);
-        addList(adj, 2, 6);
+        System.out.print("Enter number of edges: ");
+        int edges = scn.nextInt();
 
-        boolean[] vis = new boolean[rows + 1];
+        System.out.println("Enter edges (u v):");
+        for (int i = 0; i < edges; i++) {
+
+            int u = scn.nextInt();
+            int v = scn.nextInt();
+
+            if (u < 1 || u > nodes || v < 1 || v > nodes) {
+                System.out.println("Invalid edge. Try again.");
+                i--;
+                continue;
+            }
+
+            addEdge(adj, u, v);
+        }
+
+        System.out.print("Enter starting node: ");
+        int start = scn.nextInt();
+
+        if (start < 1 || start > nodes) {
+            throw new IllegalArgumentException(
+                    "Starting node must be between 1 and " + nodes);
+        }
+
+        boolean[] vis = new boolean[nodes + 1];
         ArrayList<Integer> bfsResult = new ArrayList<>();
         Queue<Integer> q = new LinkedList<>();
 
-        bfs(adj, vis, 1, q, bfsResult);
+        bfs(adj, vis, start, q, bfsResult); 
 
-        for (int i = 0; i < bfsResult.size(); i++) {
-            System.out.print(bfsResult.get(i)+" ");
+        System.out.println("BFS Traversal:");
+        for (int node : bfsResult) {
+            System.out.print(node + " ");
         }
+
+        scn.close();  
     }
 
-    public static void addList(ArrayList<ArrayList<Integer>> arr, int u, int v) {
+    public static void addEdge(ArrayList<ArrayList<Integer>> arr, int u, int v) {
         arr.get(u).add(v);
         arr.get(v).add(u);
     }
 
-    public static void bfs(ArrayList<ArrayList<Integer>> adj, boolean[] vis, int startingNode, Queue<Integer> q,
-            ArrayList<Integer> bfsResult) {
+    public static void bfs(ArrayList<ArrayList<Integer>> adj,
+                           boolean[] vis,
+                           int startingNode,
+                           Queue<Integer> q,
+                           ArrayList<Integer> bfsResult) {
+
         vis[startingNode] = true;
         q.add(startingNode);
-        
+
         while (!q.isEmpty()) {
             int node = q.poll();
             bfsResult.add(node);
-            for (int i : adj.get(node)) {
-                if (!vis[i]) {
-                    vis[i] = true;
-                    q.add(i);
+
+            for (int neighbour : adj.get(node)) {
+                if (!vis[neighbour]) {
+                    vis[neighbour] = true;
+                    q.add(neighbour);
                 }
             }
         }
--- graph/DFS.java
+++ graph/DFS.java
@@ -4,48 +4,81 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DFS {
-    public static void main(String[] args) {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        Scanner scn = new Scanner(System.in);
-        System.out.println("Enter number of vertices: ");
-        int rows = scn.nextInt();
-        scn.close();
 
-        for (int i = 0; i <= rows; i++) {
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Enter number of vertices: ");
+        int nodes = scn.nextInt();
+
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        for (int i = 0; i <= nodes; i++) {
             adj.add(new ArrayList<>());
         }
 
-        addList(adj, 1, 2);
-        addList(adj, 1, 4);
-        addList(adj, 3, 4);
-        addList(adj, 2, 3);
-        addList(adj, 7, 2);
-        addList(adj, 5, 4);
-        addList(adj, 3, 7);
-        addList(adj, 2, 6);
+        System.out.print("Enter number of edges: ");
+        int edges = scn.nextInt();
 
-        boolean[] vis = new boolean[rows + 1];
+        System.out.println("Enter edges (u v):");
+        for (int i = 0; i < edges; i++) {
+
+            int u = scn.nextInt();
+            int v = scn.nextInt();
+
+            if (u < 1 || u > nodes || v < 1 || v > nodes) {
+                System.out.println("Invalid edge. Try again.");
+                i--;
+                continue;
+            }
+
+            addEdge(adj, u, v);
+        }
+
+        System.out.print("Enter starting node: ");
+        int start = scn.nextInt();
+
+        if (start < 1 || start > nodes) {
+            throw new IllegalArgumentException(
+                "Starting node must be between 1 and " + nodes
+            );
+        }
+
+        boolean[] vis = new boolean[nodes + 1];
         ArrayList<Integer> dfsResult = new ArrayList<>();
 
-        dfs(adj, vis, 1, dfsResult);
+        dfs(adj, vis, start, dfsResult, nodes);
 
-        for (int i = 0; i < dfsResult.size(); i++) {
-            System.out.print(dfsResult.get(i));
+        System.out.println("DFS Traversal:");
+        for (int node : dfsResult) {
+            System.out.print(node + " ");
         }
+
+        scn.close();  
     }
 
-    public static void addList(ArrayList<ArrayList<Integer>> arr, int u, int v) {
+    public static void addEdge(ArrayList<ArrayList<Integer>> arr, int u, int v) {
         arr.get(u).add(v);
         arr.get(v).add(u);
     }
 
-    public static void dfs(ArrayList<ArrayList<Integer>> adj, boolean[] vis, int startingNode,
-            ArrayList<Integer> dfsResult) {
+    public static void dfs(ArrayList<ArrayList<Integer>> adj,
+                           boolean[] vis,
+                           int startingNode,
+                           ArrayList<Integer> dfsResult,
+                           int nodes) {
+
+        if (startingNode < 1 || startingNode > nodes) {
+            throw new IllegalArgumentException("Invalid starting node");
+        }
+
         vis[startingNode] = true;
         dfsResult.add(startingNode);
-        for (int i : adj.get(startingNode)) {
-            if (!vis[i]) {
-                dfs(adj, vis, i, dfsResult);
+
+        for (int neighbour : adj.get(startingNode)) {
+            if (!vis[neighbour]) {
+                dfs(adj, vis, neighbour, dfsResult, nodes);
             }
         }
     }
