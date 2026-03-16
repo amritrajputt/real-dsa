acc30bd16407f3858845420c8388125c8e9c4fb8
amrit <vishal179singh@gmail.com>
2026-03-16 23:28:49 +0530

    add

--- graph/AdjacencyList.java
+++ graph/AdjacencyList.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class AdjacencyList {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the number of node: ");
         int node = sc.nextInt();
