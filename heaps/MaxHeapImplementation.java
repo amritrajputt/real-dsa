35ed81a1e85f50b719619a6a44450f858a477461
amrit <vishal179singh@gmail.com>
2026-03-26 22:02:36 +0530

    maxheap and minheap

--- heaps/MaxHeapImplementation.java
+++ heaps/MaxHeapImplementation.java
@@ -1,10 +1,11 @@
 package heaps;
 
-public class MinHeapImplementation {
+public class MaxHeapImplementation {
+   
     int heap[];
     int size;
 
-    public MinHeapImplementation(int capacity) {
+    public MaxHeapImplementation(int capacity) {
         heap = new int[capacity + 1];
         size = 0;
     }
@@ -13,7 +14,7 @@ public class MinHeapImplementation {
         size++;
         heap[size] = val;
         int i = size;
-        while (i > 1 && heap[i] < heap[i / 2]) {
+        while (i > 1 && heap[i] > heap[i / 2]) {
             swap(i, i / 2);
             i = i / 2;
         }
@@ -22,23 +23,23 @@ public class MinHeapImplementation {
     public int remove() {
         if (size == 0)
             return -1;
-        int min = heap[1];
+        int max = heap[1];
         heap[1] = heap[size];
         size--;
         heapifyDown(1);
-        return min;
+        return max;
     }
 
     private void heapifyDown(int i) {
         while (2 * i <= size) {
             int left = 2 * i, right = 2 * i + 1;
-            int smallest = left;
-            if(right <= size && heap[right] < heap[left]){
-                smallest = right;
+            int largest = left;
+            if(right <= size && heap[right] > heap[left]){
+                largest = right;
             }
-            if(heap[i]>heap[smallest]){
-                swap(i, smallest);
-                i = smallest;
+            if(heap[i]<heap[largest]){
+                swap(i, largest);
+                i = largest;
             }else break;
         }
     }
